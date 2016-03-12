67395b9b3082d4be48359cf2cedc7fcc94d40ac8
Saghm Rossi <saghmrossi@gmail.com>
2016-03-12 00:45:18 -0500

    remove unnecessary whitespace from display

--- src/seq.rs
+++ src/seq.rs
@@ -16,12 +16,12 @@ pub enum SeqElem {
 impl Display for SeqElem {
     fn fmt(&self, mut fmt: &mut Formatter) -> Result<(), Error> {
         match *self {
-            SeqElem::Plus(i) if i < 0 => write!(fmt, "- {}", i.abs()),
-            SeqElem::Plus(i) => write!(fmt, "+ {}", i),
-            SeqElem::Mult(i) => write!(fmt, "* {}", i),
-            SeqElem::Div(i) => write!(fmt, "/ {}", i),
-            SeqElem::Mod(i) => write!(fmt, "% {}", i),
-            SeqElem::Const(i) => write!(fmt, "= {}", i),
+            SeqElem::Plus(i) if i < 0 => write!(fmt, "-{}", i.abs()),
+            SeqElem::Plus(i) => write!(fmt, "+{}", i),
+            SeqElem::Mult(i) => write!(fmt, "*{}", i),
+            SeqElem::Div(i) => write!(fmt, "/{}", i),
+            SeqElem::Mod(i) => write!(fmt, "%{}", i),
+            SeqElem::Const(i) => write!(fmt, "={}", i),
         }
     }
 }
@@ -68,45 +68,45 @@ mod tests {
 
     #[test]
     fn fmt_seq_elem_plus() {
-        assert_eq!("+ 0", format!("{}", Plus(0)));
-        assert_eq!("+ 4", format!("{}", Plus(4)));
+        assert_eq!("+0", format!("{}", Plus(0)));
+        assert_eq!("+4", format!("{}", Plus(4)));
     }
 
     #[test]
     fn fmt_seq_elem_minus() {
-        assert_eq!("- 1", format!("{}", Plus(-1)));
-        assert_eq!("- 4", format!("{}", Plus(-4)));
+        assert_eq!("-1", format!("{}", Plus(-1)));
+        assert_eq!("-4", format!("{}", Plus(-4)));
     }
 
     #[test]
     fn fmt_seq_elem_mult() {
-        assert_eq!("* -4", format!("{}", Mult(-4)));
-        assert_eq!("* 4", format!("{}", Mult(4)));
+        assert_eq!("*-4", format!("{}", Mult(-4)));
+        assert_eq!("*4", format!("{}", Mult(4)));
     }
 
     #[test]
     fn fmt_seq_elem_div() {
-        assert_eq!("/ -4", format!("{}", Div(-4)));
-        assert_eq!("/ 4", format!("{}", Div(4)));
+        assert_eq!("/-4", format!("{}", Div(-4)));
+        assert_eq!("/4", format!("{}", Div(4)));
     }
 
     #[test]
     fn fmt_seq_elem_mod() {
-        assert_eq!("% -4", format!("{}", Mod(-4)));
-        assert_eq!("% 4", format!("{}", Mod(4)));
+        assert_eq!("%-4", format!("{}", Mod(-4)));
+        assert_eq!("%4", format!("{}", Mod(4)));
     }
 
     #[test]
     fn fmt_seq_elem_const() {
-        assert_eq!("= -4", format!("{}", Const(-4)));
-        assert_eq!("= 4", format!("{}", Const(4)));
+        assert_eq!("=-4", format!("{}", Const(-4)));
+        assert_eq!("=4", format!("{}", Const(4)));
     }
 
     #[test]
     fn fmt_seq() {
         assert_eq!("", format!("{}", Seq(Vec::new())));
-        assert_eq!("+ 4", format!("{}", Seq(vec![Plus(4)])));
-        assert_eq!("+ 4, % -6", format!("{}", Seq::new(vec![Plus(4), Mod(-6)])));
-        assert_eq!("+ 4, % -6, - 12, * 42, / 3, = 9", format!("{}", Seq(vec![Plus(4), Mod(-6), Plus(-12), Mult(42), Div(3), Const(9)])));
+        assert_eq!("+4", format!("{}", Seq(vec![Plus(4)])));
+        assert_eq!("+4, %-6", format!("{}", Seq::new(vec![Plus(4), Mod(-6)])));
+        assert_eq!("+4, %-6, -12, *42, /3, =9", format!("{}", Seq(vec![Plus(4), Mod(-6), Plus(-12), Mult(42), Div(3), Const(9)])));
     }
 }
