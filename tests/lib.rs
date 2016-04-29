6be12b755345c1798240a83c43a426dce07fd194
Saghm Rossi <saghmrossi@gmail.com>
2016-04-29 19:41:39 -0400

    add tests for meta patterns

--- tests/lib.rs
+++ tests/lib.rs
@@ -2,4 +2,5 @@
 extern crate sea_canal;
 
 mod custom;
+mod meta;
 mod standard;
---
+++ tests/meta/mod.rs
@@ -0,0 +1,25 @@
+use sea_canal::Analyzer;
+use sea_canal::Pattern;
+use sea_canal::PatternElem::*;
+
+#[test]
+fn meta_find_any_pattern() {
+    let slice = &[1, 2, 4, 7, 11];
+    let analyzer = Analyzer::with_meta(slice);
+
+    assert_eq!(Some(pat!(Meta(pat!(Plus(1), Plus(2), Plus(3), Plus(4))))), analyzer.find_any_pattern(1));
+}
+
+#[test]
+fn meta_find_patterns_of_length() {
+    let slice = &[10, 11, 10, 12, 10, 13, 10];
+    let analyzer = Analyzer::with_meta(slice);
+
+    assert_eq!(Vec::<Pattern>::new(), analyzer.find_patterns_of_length(1));
+    assert_eq!(
+        vec![
+            pat!(Meta(pat!(Plus(1), Plus(2), Plus(3))), Const(10)),
+            pat!(Meta(pat!(Plus(1), Plus(2), Plus(3))), Meta(pat!(Plus(-1), Plus(-2), Plus(-3))))
+        ],
+        analyzer.find_patterns_of_length(2));
+}
